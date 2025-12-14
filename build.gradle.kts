73c0ff808fd42b04a24b01c2cc7a5ea1a379dade
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2025-12-14 19:54:46 +0100

    fix kotlin and ksp version incompatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.0"
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     alias(libs.plugins.android.library) apply false
     kotlin("plugin.serialization") version "2.1.0"
 }
